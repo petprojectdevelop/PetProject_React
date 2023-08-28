567c225be886ae4d7e17e2734d071f14db84961f
petprojectdevelop <142349301+petprojectdevelop@users.noreply.github.com>
2023-08-28 22:01:59 +0300

    Dummy change

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -9,6 +9,7 @@ function App() {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
+          <p>Just dummy update to see how changes are being applied.</p>
           <a
               className="App-link"
               href="https://reactjs.org"
